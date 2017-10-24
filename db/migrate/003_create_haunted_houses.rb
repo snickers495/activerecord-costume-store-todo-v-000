9a24823522a762c273d2fe0fe9d5c873e532ce68
Lisa Lee <snickers495@gmail.com>
2017-10-24 19:47:49 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -7,7 +7,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.string :theme
       t.float :price
       t.boolean :family_friendly
-      t.datetime :opening_date 
+      t.datetime :opening_date
       t.datetime :closing_date
       t.string :description
     end
